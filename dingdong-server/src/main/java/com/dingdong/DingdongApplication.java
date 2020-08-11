da11bf6601df5426c542eae1d285992cf434036d
pujiulin <pujiulin@toothfish.cn>
2020-08-12 00:30:00 +0800

    mybatis报错

--- dingdong-server/src/main/java/com/dingdong/DingdongApplication.java
+++ dingdong-server/src/main/java/com/dingdong/DingdongApplication.java
@@ -1,5 +1,6 @@
 package com.dingdong;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {
         "com.dingdong.*"
 })
+@MapperScan("com.dingdong.mapper")
 public class DingdongApplication {
 
     public static void main(String[] args) {
