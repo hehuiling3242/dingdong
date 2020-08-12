6d04f4bcfbe6cc2c0980f78f3da72dc47da21048
pujiulin <pujiulin@toothfish.cn>
2020-08-12 19:05:52 +0800

    开启swagger

---
+++ dingdong-server/src/main/java/com/dingdong/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.dingdong.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2 // Swagger的开关，表示已经启用Swagger
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("dingdong项目接口文档")
+                .version("1.0")
+                .build();
+    }
+}
